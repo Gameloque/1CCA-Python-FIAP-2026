c40e85d327bea0ed0c308cf660cdc083d097b549
gameloque <gabrielhenriqueff1448@gmail.com>
2026-03-20 11:31:35 -0300

    Aula 2 - Variáveis e operadores

---
+++ Aula02a/aula02.py
@@ -0,0 +1,35 @@
+
+
+print(4 + 4)
+print( '4 + 4')
+print('4' + '4')
+
+
+
+
+ # Comentários muehheeheheh
+'''
+comentario maneiro
+'''
+
+
+ # VARIÁVEIS
+
+nome = "Gabriel" # str
+idade = 17 # int
+peso = 84.3 # float
+
+print(nome, idade, peso)
+print(f'Oi {nome}!!!')
+
+#INPUT - SIMULAÇÃO DE UM FORMS NO CMD
+
+nome = input('Digite o seu nome: ')
+idade = int(input('Digite a sua idade: '))
+
+nova_idade = idade + 1
+
+print(nome, idade)
+print(nova_idade)
+
+
---
+++ Exercícios/exerciciospy.py
@@ -0,0 +1,7 @@
+nome = input("Digite o seu nome: ")
+
+print(f'Seja bem vindo {nome}!')
+
+anodenascimento = int(input(" Digite sua data de nascimento: "))
+
+print(anodenascimento)
