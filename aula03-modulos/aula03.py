2c4e3150cc3cfe8a4a3064acf311943ec2c783ea
gameloque <gabrielhenriqueff1448@gmail.com>
2026-04-10 11:25:05 -0300

    Aula03-Coisas_Legais

---
+++ aula03-modulos/aula03.py
@@ -0,0 +1,72 @@
+import math
+import random
+
+num = 4
+raiz = math.sqrt(num)
+print(f"A raiz de {num} é {raiz:.2f}")
+
+num_rand_int = random. randint(1, 100)
+print(num_rand_int)
+
+def print_lyrics():
+    print("I ain1t gonna live forever")
+    print("I just wanna to live while I'm alive")
+
+print_lyrics()
+print() # pular linha
+
+def boas_vindas(nome):
+    print(f'Olá, {nome}! Seja bem-vindo!')
+nome_digitado = input('Digite seu nome: ')
+boas_vindas(nome_digitado)
+
+def soma(Na, Nb):
+    soma = Na + Nb
+    return soma
+
+resultado = soma(12, 4)
+print(resultado)
+print(type(nome_digitado))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
